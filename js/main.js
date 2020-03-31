50a1161cbf26a0c230a9001cce64cc46316de67a
Galina <galina.bartkova.mir@gmail.com>
2020-03-31 18:22:19 +0300

    task-1

---
+++ js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var name = "Galya";
+let age = 23;
+var boolean = "true";
+var obj = {
+    name: "Maks",
+    age: 24
+};
+var arr = ["Galinka", 25, true];
+var symb = Symbol();
+
+console.log(arr[1]);
+console.log(symb);
+
+console.log(obj.name + obj.age);
